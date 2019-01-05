59048dca5f07dcf5654eccb3cf335a70611de297
Diego Peres <speres.diego@gmail.com>
2019-01-05 21:54:49 -0200

    Setting up listeners

--- betbright/application.py
+++ betbright/application.py
@@ -1,17 +1,38 @@
+import aioredis
+from motor.motor_asyncio import AsyncIOMotorClient
 from sanic import Sanic
 
 from betbright.config import settings
 
 
 class Application(Sanic):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.config.update(settings)
-
     def run(self, *args, **kwargs):
-        self.config.app.update(kwargs)
-        super().run(*args, **self.config.app)
+        settings['app'].update(kwargs)
+        super().run(*args, **settings['app'])
 
 
 app = Application()
+
+
+@app.listener('before_server_start')
+def settings_update(app, loop):
+    app.config.update(settings)
+
+
+@app.listener('before_server_start')
+def mongodb_setup(app, loop):
+    uri = app.config.mongo['uri']
+    database = app.config.mongo['database']
+    app.mongo = AsyncIOMotorClient(uri, io_loop=loop)[database]
+
+
+@app.listener('before_server_start')
+async def redis_setup(app, loop):
+    uri = app.config.redis['uri']
+    app.redis = await aioredis.create_pool(uri, minsize=5, loop=loop)
+
+
+@app.listener('before_server_stop')
+async def redis_teardown(app, loop):
+    app.redis.close()
+    await app.redis.wait_closed()
