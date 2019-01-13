583236ff7915af264fee87a4e43a4371467379e0
Diego Peres <speres.diego@gmail.com>
2019-01-13 15:46:14 -0200

    Loading settings by environments

--- betbright/config/__init__.py
+++ betbright/config/__init__.py
@@ -1,5 +1,8 @@
+import os
 import yaml
 
-_yaml_path = 'betbright/config/local.yml'
+_ENVINRONMENT = os.environ.get('ENV', 'local')
+_yaml_path = 'betbright/config/{0}.yml'.format(_ENVINRONMENT)
+
 with open(_yaml_path, 'r') as file:
     settings = yaml.load(file)
